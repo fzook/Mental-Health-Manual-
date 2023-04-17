2573bc28e23759b2b167f470cc497c1a22c9825b
Forre Zook <fzook@uccs.edu>
2023-04-17 16:38:04 -0600

    background image

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Swiper slider
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".bg-slider-thumbs", {
     loop: true,
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
   });
-  var swiper2 = new Swiper(".mySwiper2", {
+  var swiper2 = new Swiper(".bg-slider", {
     loop: true,
     spaceBetween: 10,
     navigation: {
