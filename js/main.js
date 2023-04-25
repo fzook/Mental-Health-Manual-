469cdf463eafe227e58f82f816d6be383102de9f
Forre Zook <fzook@uccs.edu>
2023-04-25 14:21:43 -0600

    change section based on slide

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
+(function() {
+    document.getElementById('section-1').style.display = "none"
+    document.getElementById('section-2').style.display = "none"
+    document.getElementById('section-3').style.display = "none"
+})();
 // Swiper slider
-var swiper = new Swiper(".bg-slider-thumbs", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".bg-slider", {
+
+  var swiper = new Swiper(".bg-slider", {
     loop: true,
     spaceBetween: 10,
     navigation: {
@@ -16,4 +15,48 @@ var swiper = new Swiper(".bg-slider-thumbs", {
     thumbs: {
       swiper: swiper,
     },
+  });
+
+  swiper.on('slideChange', function (e) {
+    if(e.realIndex === 0) {
+        document.getElementById('section-1').style.display = "none"
+        document.getElementById('section-2').style.display = "none"
+        document.getElementById('section-3').style.display = "none"
+        document.getElementById('section-0').style.display = "block"
+    } else if (e.realIndex === 1) {
+        document.getElementById('section-0').style.display = "none"
+        document.getElementById('section-2').style.display = "none"
+        document.getElementById('section-3').style.display = "none"
+        document.getElementById('section-1').style.display = "block"
+    } else if (e.realIndex === 2) {
+        document.getElementById('section-1').style.display = "none"
+        document.getElementById('section-0').style.display = "none"
+        document.getElementById('section-3').style.display = "none"
+        document.getElementById('section-2').style.display = "block"
+    } else {
+        document.getElementById('section-1').style.display = "none"
+        document.getElementById('section-2').style.display = "none"
+        document.getElementById('section-0').style.display = "none"
+        document.getElementById('section-3').style.display = "block"
+    }
+
+});
+
+  //Navigation ba effects on scroll
+  window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+
+  //Responsive navigation menu toggle
+  const menuBtn = document.querySelector(".nav-menu-btn");
+  const closeBtn = document.querySelector(".nav-close-btn");
+  const navigation = document.querySelector(".navigation");
+
+  menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
   });
